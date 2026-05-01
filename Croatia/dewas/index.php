c10f608880f7f976a3a0db9867aa237d4177f2b1
Teja Reddy <76819847+Tejareddy4@users.noreply.github.com>
2026-05-01 16:26:50 +0530

    Updated all index pages related posts url structure

--- Croatia/dewas/index.php
+++ Croatia/dewas/index.php
@@ -9,7 +9,7 @@ $Updatedhtml = <<<'UPDATEDHTML'
 <p>IEC: Exporters must adhere to International Electrotechnical Commission (IEC) standards to ensure product safety and interoperability. Compliance demonstrates a commitment to quality and facilitates market access globally. Understanding specific IEC requirements for their product category is crucial for successful international trade and avoiding costly non-compliance issues.</p>
 UPDATEDHTML;
 $Relposts = <<<'RELPOSTS'
-<h4>Explore more delivery options from your city</h4><ul><li><a href="https://shipglobal.in/service/ludhiana-to-croatia">International Courier Service from Ludhiana To Croatia</a></li><li><a href="https://shipglobal.in/service/amritsar-to-croatia">International Courier Service from Amritsar To Croatia</a></li><li><a href="https://shipglobal.in/service/jalandhar-to-croatia">International Courier Service from Jalandhar To Croatia</a></li><li><a href="https://shipglobal.in/service/mohali-to-croatia">International Courier Service from Mohali To Croatia</a></li></ul><h2>FAQs for International Courier Service from Dewas to Croatia</h2>
+<h4>Explore more delivery options from your city</h4><ul><li><a href="https://shipglobal.in/services/Croatia/ludhiana/">International Courier Service from Ludhiana To Croatia</a></li><li><a href="https://shipglobal.in/services/Croatia/amritsar/">International Courier Service from Amritsar To Croatia</a></li><li><a href="https://shipglobal.in/services/Croatia/jalandhar/">International Courier Service from Jalandhar To Croatia</a></li><li><a href="https://shipglobal.in/services/Croatia/mohali/">International Courier Service from Mohali To Croatia</a></li></ul><h2>FAQs for International Courier Service from Dewas to Croatia</h2>
 RELPOSTS;
 $FAQ = <<<'FAQ'
 
--- Croatia/patiala/index.php
+++ Croatia/patiala/index.php
@@ -9,7 +9,7 @@ $Updatedhtml = <<<'UPDATEDHTML'
 
 UPDATEDHTML;
 $Relposts = <<<'RELPOSTS'
-<h4>Explore more delivery options from your city</h4><ul><li><a href="https://shipglobal.in/service/ahmedabad-to-czech-republic">International Courier Service from Ahmedabad To Czech Republic</a></li><li><a href="https://shipglobal.in/service/surat-to-czech-republic">International Courier Service from Surat To Czech Republic</a></li><li><a href="https://shipglobal.in/service/vadodara-to-czech-republic">International Courier Service from Vadodara To Czech Republic</a></li><li><a href="https://shipglobal.in/service/rajkot-to-czech-republic">International Courier Service from Rajkot To Czech Republic</a></li></ul><h2>FAQs for Top International Courier Service from Patiala to Croatia</h2>
+<h4>Explore more delivery options from your city</h4><ul><li><a href="https://shipglobal.in/services/Croatia/ahmedabad/">International Courier Service from Ahmedabad To Czech Republic</a></li><li><a href="https://shipglobal.in/services/Croatia/surat/">International Courier Service from Surat To Czech Republic</a></li><li><a href="https://shipglobal.in/services/Croatia/vadodara/">International Courier Service from Vadodara To Czech Republic</a></li><li><a href="https://shipglobal.in/services/Croatia/rajkot/">International Courier Service from Rajkot To Czech Republic</a></li></ul><h2>FAQs for Top International Courier Service from Patiala to Croatia</h2>
 RELPOSTS;
 $FAQ = <<<'FAQ'
 
